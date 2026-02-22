e526d61d4a9820228bac154c18c6890e6bf8a583
Ham Sphere <elainexyz2006@gmail.com>
2026-02-22 18:19:08 -0500

    graphing support for non-linear functions & function class refactor

--- riemann/src/main/java/com/ham/Function.java
+++ riemann/src/main/java/com/ham/Function.java
@@ -20,27 +20,9 @@ public class Function {
     }
     public Function(String expression)
     {
-        //this is a vibe coded support for implicit multiplication
-            // Add implicit multiplication: 2x becomes 2*x
-            expression = expression.replaceAll("(\\d)([a-zA-Z])", "$1*$2");
-            // Add implicit multiplication: )x becomes )*x
-            expression = expression.replaceAll("(\\))([a-zA-Z])", "$1*$2");
-            // Add implicit multiplication: x( becomes x*(
-            expression = expression.replaceAll("([a-zA-Z])(\\()", "$1*$2");
-        
-        this.expression = expression;
+        this.expression = processExpression(expression);
         this.manager = new ScriptEngineManager();
-
-        //another vibe coded support for nashorn deprecation in java 15
-            this.engine = manager.getEngineByName("nashorn");
-            if (this.engine == null) {
-                this.engine = manager.getEngineByName("JavaScript");
-            }
-            if (this.engine == null) {
-                System.err.println("ERROR: JavaScript engine not available!");
-            } else {
-                System.out.println("JavaScript engine loaded: " + this.expression);
-            }
+        this.engine = manager.getEngineByName("JavaScript");
     }
 
     public double getY(double x)
@@ -52,9 +34,45 @@ public class Function {
             return Double.NaN;
         }
     }
-    public String replaceX(double x)
+    private String replaceX(double x)
     {
         String xValue = "(" + Double.toString(x) + ")";
         return this.expression.replaceAll("x", xValue);
     }
+
+    public String getExpression()
+    {
+        return expression;
+    }
+    public void setExpression(String expression)
+    {
+        expression = processExpression(expression);
+    }
+
+    private String processExpression(String expression)
+    {
+        //this is a vibe coded support for string parsing
+            // Add implicit multiplication: 2x becomes 2*x
+            expression = expression.replaceAll("(\\d)([a-zA-Z])", "$1*$2");
+            // Add implicit multiplication: )x becomes )*x
+            expression = expression.replaceAll("(\\))([a-zA-Z])", "$1*$2");
+            // Add implicit multiplication: x( becomes x*(
+            expression = expression.replaceAll("([a-zA-Z])(\\()", "$1*$2");
+        
+            // Convert mathematical notation to JavaScript:
+            // x^2 becomes Math.pow(x, 2) (Nashorn doesn't support ** operator)
+            expression = expression.replaceAll("([a-zA-Z0-9)]+)\\^([a-zA-Z0-9(]+)", "Math.pow($1,$2)");
+            
+            // Add Math. prefix to trig and math functions
+            expression = expression.replaceAll("\\bsin\\(", "Math.sin(");
+            expression = expression.replaceAll("\\bcos\\(", "Math.cos(");
+            expression = expression.replaceAll("\\btan\\(", "Math.tan(");
+            expression = expression.replaceAll("\\bsqrt\\(", "Math.sqrt(");
+            expression = expression.replaceAll("\\babs\\(", "Math.abs(");
+        
+            System.out.println("Converted to: " + expression);
+        
+        return expression;
+    }
+
 }
--- riemann/src/main/java/com/ham/Main.java
+++ riemann/src/main/java/com/ham/Main.java
@@ -5,7 +5,7 @@ import codedraw.CodeDraw;
 import codedraw.Palette;
 // run on POWERSHELL:
 // (cd to riemann)
-// mvn clean compile exec:java "-Dexec.mainClass=com.ham.Main"
+// mvn compile exec:java "-Dexec.mainClass=com.ham.Main"
 public class Main {
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
@@ -16,15 +16,16 @@ public class Main {
     {  
 
         CodeDraw cd = new CodeDraw(WIDTH, HEIGHT);
-        Function f = new Function("-2x+3");
+        Function f = new Function("x^3");
         
-        drawGrid(X_OFFSET, Y_OFFSET, cd);
-        drawFunction(f, X_OFFSET, Y_OFFSET, cd);
+        drawGrid(cd);
+        drawFunction(f, cd);
+        //drawRS("right", 0, 8, 8, f, cd);
         
         cd.show();
     }
 
-    public static void drawGrid(int xOff, int yOff, CodeDraw cd)
+    public static void drawGrid(CodeDraw cd)
     {
         //reset canvas
         cd.clear();
@@ -39,31 +40,66 @@ public class Main {
         for (int i = -WIDTH/2; i < WIDTH/2; i += SCALE)
         {
             if (i != 0) {  //skip y-axis
-                cd.drawLine(i + xOff, -HEIGHT/2 + yOff, i + xOff, HEIGHT/2 + yOff);
+                cd.drawLine(i + X_OFFSET, -HEIGHT/2 + Y_OFFSET, i + X_OFFSET, HEIGHT/2 + Y_OFFSET);
             }
         }
         for (int i = -HEIGHT/2; i < HEIGHT/2; i += SCALE)
         {
             if (i != 0) {  //skip x-axis
-                cd.drawLine(-WIDTH/2 + xOff, i + yOff, WIDTH/2 + xOff, i + yOff);
+                cd.drawLine(-WIDTH/2 + X_OFFSET, i + Y_OFFSET, WIDTH/2 + X_OFFSET, i + Y_OFFSET);
             }
         }
     }
 
-    public static void drawFunction(Function f, int xOff, int yOff, CodeDraw cd)
+    public static void drawFunction(Function f, CodeDraw cd)
     {
+        double[] prevPoint = new double[2];
+        double[] curPoint = new double[2];
         cd.setColor(Palette.GREEN);
         for (int i = -WIDTH/2; i < WIDTH/2; i++)
         {
             double x = i / SCALE;  //convert screen coordinate to math coordinate
             double y = f.getY(x);
-            cd.drawPoint(i + xOff, -(y * SCALE) + yOff);  //convert back to screen coordinates
+            
+            prevPoint = curPoint;
+            curPoint = new double[2];
+            curPoint[0] = i + X_OFFSET;
+            curPoint[1] = -(y * SCALE) + Y_OFFSET;
+            
+            // public void drawLine(double startX, double startY, double endX, double endY)
+            if (i != -WIDTH/2)
+            {
+                cd.drawLine(prevPoint[0], prevPoint[1], curPoint[0], curPoint[1]);
+            }
+                 
+            cd.drawPoint(i + X_OFFSET, -(y * SCALE) + Y_OFFSET);  //convert back to screen coordinates
         }
     }
 
-    public static void drawRS(String approxType, double a, double b, int n, Function f, int xOff, int yOff, CodeDraw cd)
+    //fillRectangle(double x, double y, double width, double height)
+    public static void drawRS(String approxType, double a, double b, int n, Function f, CodeDraw cd)
     {
+        if (approxType.equals("right"))
+        {
+            for (int i = 0; i < n; i++)
+            {
+                //cd.fillRectangle(1,1 ,1 ,1 );
+            }
+        }
+        else if (approxType.equals("left"))
+        {
+            for (int i = 0; i < n; i++)
+            {
 
+            }
+        }
+        else if (approxType.equals("mid"))
+        {
+            for (int i = 0; i < n; i++)
+            {
+
+            }
+        }
     }
 
 }
